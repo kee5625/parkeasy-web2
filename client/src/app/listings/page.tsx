00eefd57faafcea80c5ff06f07c9ff38607b7354
Karthik R <karthikeya.rachamolla@gmail.com>
2025-09-05 03:05:36 -0400

    added default modal component

--- client/src/app/listings/page.tsx
+++ client/src/app/listings/page.tsx
@@ -2,8 +2,10 @@
 
 import React from 'react'
 import Listing from '@/components/listing'; 
-import { Box, List, Paper, styled } from '@mui/material'; 
+import { Box, List, Paper, styled, Typography, Backdrop, Modal, Button } from '@mui/material'; 
+import { useSpring, animated } from '@react-spring/web';
 import Grid from '@mui/material/Grid';
+import CardActionArea from '@mui/material/CardActionArea';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
@@ -57,6 +59,59 @@ const dummyData = {
 
 const listingsArray = Object.values(dummyData);
 
+interface FadeProps {
+  children: React.ReactElement<any>;
+  in?: boolean;
+  onClick?: any;
+  onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
+  onExited?: (node: HTMLElement, isAppearing: boolean) => void;
+  ownerState?: any;
+}
+
+const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(props, ref) {
+  const {
+    children,
+    in: open,
+    onClick,
+    onEnter,
+    onExited,
+    ownerState,
+    ...other
+  } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter(null as any, true);
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited(null as any, true);
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {React.cloneElement(children, { onClick })}
+    </animated.div>
+  );
+});
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const page = () => {
 
   return (
@@ -72,24 +127,58 @@ const page = () => {
 };
 
 export function ResponsiveGrid() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
-        {listingsArray.map((listing, idx) => (
-          <Grid key={idx} size={{xs: 12, sm:6, md:4}}>
-            <Item>
-              <Listing 
-                key={idx}
-                address={listing.address}
-                description={listing.description}
-                owner={listing.owner}
-                imageName={listing.imageName}
-              />
-            </Item>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            TransitionComponent: Fade,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="spring-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="spring-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center" alignItems="center">
+          {listingsArray.map((listing, idx) => (
+            <Grid key={idx} size={{xs: 12, sm:6, md:4}}>
+              <Item>
+                <CardActionArea onClick={handleOpen}>
+                  <Listing 
+                    key={idx}
+                    address={listing.address}
+                    description={listing.description}
+                    owner={listing.owner}
+                    imageName={listing.imageName}
+                  />
+                </CardActionArea>
+                
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+    
   )
 }
 
--- client/src/components/listing.tsx
+++ client/src/components/listing.tsx
@@ -5,9 +5,7 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
 import Link from 'next/link';
 
 interface ListingProps {
@@ -20,49 +18,43 @@ interface ListingProps {
 export default function Listing({address, description, owner, imageName}: ListingProps) {
   return (
     <Card sx={{ maxWidth: 345, minHeight: 400 }}>
-      <Link href="/listingsPage">
-        <CardActionArea>
-          <CardHeader
-            avatar= {
-                <CardMedia
-                component="img"
-                sx={{ width: 50, height: 50, objectFit: 'cover' }}
-                image="/defaultPFP.png"
-                />
-            }
-            title="Property Listing"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
+      <CardHeader
+        avatar= {
+            <CardMedia
             component="img"
-            height="194"
-            image={imageName}
-            alt="House images"
-          />
-          <CardContent>
-            <Typography variant="body2" sx={{   color: 'text.secondary' }}>
-                Address: {address}
-            </Typography>
-            <Typography variant="body2" sx={{   color: 'text.secondary' }}>
-                Description: {description}
-            </Typography>
-            <Typography variant="body2" sx={{   color: 'text.secondary' }}>
-                Hosted By: {owner}
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            {/*
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>*/}
-          </CardActions>
-        </CardActionArea>
-      </Link>
-      
-      
+            sx={{ width: 50, height: 50, objectFit: 'cover' }}
+            image="/defaultPFP.png"
+            />
+        }
+        title="Property Listing"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={imageName}
+        alt="House images"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{   color: 'text.secondary' }}>
+            Address: {address}
+        </Typography>
+        <Typography variant="body2" sx={{   color: 'text.secondary' }}>
+            Description: {description}
+        </Typography>
+        <Typography variant="body2" sx={{   color: 'text.secondary' }}>
+            Hosted By: {owner}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/*
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>*/}
+      </CardActions>      
     </Card>
   );
 }
